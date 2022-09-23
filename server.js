65f6653f5bebfdf50043631a760c91520140bf7f
ifotn <rich@infrontofthenet.com>
2022-09-23 16:26:13 -0400

    l2/2 - express w/multiple urls

--- server.js
+++ server.js
@@ -3,15 +3,37 @@ const express = require('express')
 // create new express object.  "new" keyword not used in js
 const app = express()
 
-// add http request handler
+// add http request handlers
 let hello = (req, res) => {
     res.writeHead(200)
     res.write('Hello world')
     res.end()
 }
 
+let goodbye = (req, res) => {
+    res.writeHead(200)
+    res.write('Goodbye world')
+    res.end()
+}
+
+let index = (req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200)
+        res.write('Home Page')
+        res.end()
+    }
+    else {
+        res.writeHead(404)
+        res.write('Not Found')
+        res.end()
+    }
+}
+
 // route http requests to our handler function
-app.use(hello)
+// index path must go last or it executes on all urls
+app.use('/hello', hello)
+app.use('/goodbye', goodbye)
+app.use('/', index)
 
 // start express web server
 app.listen(3000)
